5ac1cbbda7c336e4aa843cd37fab4186800f5f00
SSK <hide.saki.wo@gmail.com>
2020-10-24 13:09:19 +0900

    いらないもの削除

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import ModalWindow from './ModalWindow';
 
 class Todo extends Component{
   render(){ 
       return(
           <div className="todos">
               {this.props.value.map(i=>{if(i.area === this.props.area)
-                  return <div className="todo" onClick={()=>{console.log(i)}}>{i.name}</div>
+                  return <div className="todo"><ModalWindow name={i.name} des={i.des}/></div>
               })}
           </div>
       );
